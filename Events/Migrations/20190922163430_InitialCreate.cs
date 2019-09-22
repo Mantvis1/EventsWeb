8431e5df55641bf02f8e5f92de7fc826abd9a87a
Mantvydas Razulevicius <razuleviciusmantvydas@gmail.com>
2019-09-22 19:48:07 +0300

    Migrations created

---
+++ Events/Migrations/20190922163430_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Events.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Event",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    title = table.Column<string>(nullable: false, maxLength: 50),
+                    summary = table.Column<string>(nullable: false, maxLength: 200)
+                }
+        );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+            migrationBuilder.DropTable(
+                name: "Event");
+
+        }
+    }
+}
--- Events/Models/Event.cs
+++ Events/Models/Event.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Events.Models
 {
+    [Table("Event")]
     public class Event
     {
-
+        [Required]
+        public int id { get; set; }
+        public string title { get; set; }
+        public string summary { get; set; }
     }
 }
