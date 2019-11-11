0a5ece9841432fdd1cfda77bdfb3e68be60583c4
Mantvydas Razulevicius <razuleviciusmantvydas@gmail.com>
2019-11-11 22:00:59 +0200

    delete user

--- EventsUI/src/app/User/user-details/user-details.component.ts
+++ EventsUI/src/app/User/user-details/user-details.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "src/app/api.service";
+import { ActivatedRoute } from "@angular/router";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-user-details',
-  templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.css']
+  selector: "app-user-details",
+  templateUrl: "./user-details.component.html",
+  styleUrls: ["./user-details.component.css"]
 })
 export class UserDetailsComponent implements OnInit {
+  id: number;
+  userInfo: any;
+  userDataForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private api: ApiService,
+    private activatedRoute: ActivatedRoute,
+    private fb: FormBuilder
+  ) {
+    this.id = this.activatedRoute.snapshot.params.id;
+    this.userDataForm = this.fb.group({
+      name: [""],
+      password: [""]
+    });
   }
 
+  getUserInfo() {
+    this.api.getUrl("users/" + this.id).subscribe(data => {
+      this.userInfo = data;
+    });
+  }
+
+  postUserInfo() {}
+
+  deleteAccount() {
+    this.api.deleteUrl("users/" + this.id).subscribe(null, error => {
+      console.error("There was an error during the request");
+      console.log(error);
+    });
+  }
+
+  ngOnInit() {
+    this.getUserInfo();
+  }
 }
