84e96a1fbd57a6438735ea73fff7ae079ff72b7a
Mantvydas Razulevicius <razuleviciusmantvydas@gmail.com>
2019-11-11 17:44:42 +0200

    user registration successful

--- EventsUI/src/app/register/register.component.ts
+++ EventsUI/src/app/register/register.component.ts
@@ -25,36 +25,45 @@ export class RegisterComponent implements OnInit {
     });
   }
 
-  register(name: string, pass: string, passRepeat: string, email: string) {
-    if (name.length == 0 || pass.length == 0) {
-      this.showError("vardas ir slaptazodis negali buti tusti");
+  register() {
+    if (
+      this.form.value.name.length == 0 ||
+      this.form.value.password.length == 0 ||
+      this.form.value.email.length == 0
+    ) {
+      this.showError("vardas,el. paštas ir slaptazodis negali buti tusti");
+    } else if (this.form.value.password == this.form.value.repeatPassword) {
+      this.addNewUser();
+    } else {
+      this.showError("Slaptazodziai nesutampa");
     }
-    if (pass == passRepeat) {
-      this.addNewUser(name, pass, email);
-    }
-    this.showError("Slaptazodziai nesutampa");
   }
 
   redirectToLogIn() {
     this.router.navigate(["/login"]);
   }
 
-  addNewUser(name: string, password: string, email: string) {
+  addNewUser() {
     this.api
-      .postUrl("register", {
-        name: name,
-        password: password,
-        email: email
+      .postUrl("auth/register", {
+        name: this.form.value.name,
+        password: this.form.value.password,
+        email: this.form.value.email
       })
-      .subscribe();
+      .subscribe(
+        res => {
+          console.log("received ok response from patch request");
+        },
+        error => {
+          console.error("There was an error during the request");
+          console.log(error);
+        }
+      );
     this.redirectToLogIn();
   }
   showError(message: string) {
-    this.error = "";
+    this.error = message;
   }
 
-  submitForm() {
-    console.log(this.form);
-  }
   ngOnInit() {}
 }
